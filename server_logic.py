6fe6122fea69900554888a1045c3458e45a5905c
jansan <100417871+jansan85@users.noreply.github.com>
2022-03-02 09:40:18 +0100

    Add files via upload

--- server_logic.py
+++ server_logic.py
@@ -93,7 +93,7 @@ def direct_move_to_eat(my_head: Dict[str, int], food_coord: [str, int]) -> str:
       print ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! PRIO - eat right")
       return "right"
     else: return "false"
-
+      
 """Improving randomness"""
 #Function to get movement direction out of dictionary having all possible moves witch chances
 def get_move_with_highest_chance(possible_moves_chances:[dict]) -> str:
@@ -105,11 +105,58 @@ def get_move_with_highest_chance(possible_moves_chances:[dict]) -> str:
       print(f" {bestmove} mit Chance {possible_moves_chances[key]} ")
   return bestmove
 
+#Function to get Coordinate for possible move
+def get_coord_for_movedirection(head: Dict[str, int], move:[str]) -> Dict[str, int]:
+    if move == "up":
+      head["y"] = (head["y"] + 1)
+      return head
+    elif move == "down":
+      head["y"] = (head["y"] - 1)
+      return head
+    elif move == "left":
+      head["x"] = (head["x"] - 1)
+      return head
+    elif move == "right":
+      head["x"] = (head["x"] + 1)
+      return head
+    else:
+      return head
+
+#Function to check if coordinate is in List of coordinates
+def is_coord_in_coordlist(coord: [str, int], coord_list: List[dict]) -> str:
+  bol_is_in_list = False
+  for i in coord_list: #iterate through all items of the list
+    if (i["x"] == coord["x"]) and (i["y"] == coord["y"]): # and set result var on true if coordinates are equal
+      bol_is_in_list = True
+  return bol_is_in_list
+
+#Function to get list of neighbor-coords for a coord
+def get_list_environmental_coords_for_coord(coord: [str, int]) -> List[dict]:
+  result_coord_list = []
+  
+  upper_coord = {"x" : coord["x"], "y" : (coord["y"]+1)}
+  upper_coord_copy = upper_coord.copy()
+  result_coord_list.append(upper_coord_copy)
+
+  lower_coord = {"x" : coord["x"], "y" : (coord["y"]-1)}
+  lower_coord_copy = lower_coord.copy()
+  result_coord_list.append(lower_coord_copy)
+
+  right_coord = {"x" : (coord["x"]+1), "y" : coord["y"]}
+  right_coord_copy = right_coord.copy()
+  result_coord_list.append(right_coord_copy)
+  
+  left_coord = {"x" : (coord["x"]-1), "y" : coord["y"]}
+  left_coord_copy = left_coord.copy()
+  result_coord_list.append(left_coord_copy)
+  
+  return result_coord_list
+      
 #Function to manipulate the chance of a movement direction in a dict move / chance
 def change_chance_of_movement(move:[str], chance_value:[int], possible_moves_chances:[dict]) -> dict:
   if move in possible_moves_chances: 
     possible_moves_chances[move] = (possible_moves_chances[move] + chance_value)
-    print(f" Increase {move} by {chance_value} ")
+    print(f" Changed {move} by {chance_value} ")
   return possible_moves_chances
 
 #Get movement direction based on near food and a ranking how far it is away - ranking by chance 100 - distance
@@ -127,6 +174,9 @@ def get_move_to_near_food(my_head: Dict[str, int], food_coord: [str, int]) -> di
       print ("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FOOD is down")
       return ({"down": (100-(my_head["y"]-food_coord["y"]))})
   return ({"false": 0})
+
+def bad_idea_check(planned_coord: Dict[str, int], my_body: List[dict], board_height:[int], board_width:[int]) -> int: 
+  return 0
 
 def choose_move(data: dict) -> str:
     """
@@ -192,22 +242,38 @@ def choose_move(data: dict) -> str:
         possible_moves_chances[str_move] = 0 #create all with chance 0
         print(f" {str_move} added to move/chance list")
       
-      print(f" {possible_moves_chances} ")
+      #print(f" {possible_moves_chances} ")
 
-      # Go to food if it is horizontalyl or vertically 
+      """ Go to food if it is horizontalyl or vertically """
       for food_coord in food: #take all food coordinates
         improve_move_dic = get_move_to_near_food(my_head, food_coord) #check if it is directly vertically or horizontally and get distance
-        #print(f"improve {improve_move_dic} ")
-
-        # take over direction and distance in possible moves
-        for key in improve_move_dic:
-          if key != "false":
-            possible_moves_chances = change_chance_of_movement(key, improve_move_dic[key], possible_moves_chances)      
+        """ take over direction and distance in possible moves """
+      for key in improve_move_dic:
+        if key != "false":
+          possible_moves_chances = change_chance_of_movement(key, improve_move_dic[key], possible_moves_chances)      
        
       
+
+
+      """reduce chance of movement, when other snakes head could go on that field"""
+      for snake in snakes:
+        if (snake["name"] != "daSchnake"):
+          snake_body = snake["body"] # take other snakes body
+          for possible_move in possible_moves_chances: # for all possible moves
+            possible_coord_dic = get_coord_for_movedirection(my_head, possible_move) # get every target coord if you do the move
+            envire_pos_coords_list = []
+            envire_pos_coords_list = get_list_environmental_coords_for_coord(possible_coord_dic) # and get all neighbors to that target coord
+            near_snake_head = is_coord_in_coordlist(snake_body[0], envire_pos_coords_list) # and now check, if on on of the neighbors is the head of another snake
+            if (near_snake_head == True):
+              print(f"SNAKE is near if I go {possible_move}")
+              possible_moves_chances = change_chance_of_movement(possible_move, -100 ,possible_moves_chances) # reduce the chance by 100 if a snakes head is near
+                
+          #possible_moves = avoid_other_snakes(my_head, snake["body"], possible_moves)
+          
       """get the best move option from dict with moves and chances"""
       bestmove = get_move_with_highest_chance(possible_moves_chances) 
       print(f"Selected {bestmove} as best move")
+      
       
       move = bestmove
       #move = random.choice(possible_moves)
